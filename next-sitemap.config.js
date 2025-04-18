a7c2b72bcbad2ddfc5ba97d34c2ecdf51d0433c6
lefortnuno <trofelnuno@gmail.com>
2025-04-18 16:15:33 +0100

    remove: SEO MC - Sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://masoandrocapital.vercel.app", // ou le domaine Vercel
+  siteUrl: "https://masoandrocapital.vercel.app",
   generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: [], // exclure certaines pages
+
+  // Ajout de pages manuelles 
+  additionalPaths: async (config) => [
+    await config.transform(config, "/pages/musics"),
+    await config.transform(config, "/pages/film"),
+    await config.transform(config, "/pages/contact"),
+  ],
 };
