c83fd2e1ec995ae48f80f69cf66e82f0b1a9a74a
lefortnuno <trofelnuno@gmail.com>
2025-05-23 19:59:09 +0100

    fix: animation

--- music.data.tsx
+++ music.data.tsx
@@ -2,7 +2,7 @@ export interface VideoItem {
   thumbnail: string;
   duration: string;
   title: string;
-  channelName: string;
+  author_name: string;
   link: string;
 }
 
@@ -11,70 +11,70 @@ export const videos: VideoItem[] = [
     thumbnail: "/images/musics/pdrsr.webp",
     duration: "3:47",
     title: "SOUDARA - Pas de Repos Sans Réussite",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=2XOd2k2LOC0",
   },
   {
     thumbnail: "/images/musics/tsyHoZao.webp",
     duration: "3:29",
     title: "601 BRO - TSY HO ZAO",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=jMEF9rgG1iQ",
   },
   {
     thumbnail: "/images/musics/engaoTseky.webp",
     duration: "2:45",
     title: "601 BRO - Engao Tseky",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=aF_XP1Ey_G4",
   },
   {
     thumbnail: "/images/musics/realGasyMan.webp",
     duration: "2:19",
     title: "SOUDARÀ - REAL GASY MAN ",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=1yI0cU2LQvI",
   },
   {
     thumbnail: "/images/musics/iaSha.webp",
     duration: "4:48",
     title: "SOUDARÀ Feat DM - ꞮA SHA ?",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=lTUif9qbmDo",
   },
   {
     thumbnail: "/images/musics/femmeDeMaVie.webp",
     duration: "3:29",
     title: "601 BRO - Femme de ma vie",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=2PRCkgpSIJQ",
   },
   {
     thumbnail: "/images/musics/soaAvao.webp",
     duration: "3:35",
     title: "SOA AVAO - 601 BRO",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=gE-0sHt1kDE",
   },
   {
     thumbnail: "/images/musics/fatiavaLavitry.webp",
     duration: "3:06",
     title: "601BRO - Fitiava lavitry",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=6UzfMmHU_VA",
   },
   {
     thumbnail: "/images/musics/haiko.webp",
     duration: "4:30",
     title: "NC -Tsy Haiko sasy R&B",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=frFSjaCkRaM",
   },
   {
     thumbnail: "/images/musics/ghetto.webp",
     duration: "3:11",
     title: "YOUNG JE - BIG UP TO GHETTO MC2K21",
-    channelName: "Directed by Tony Braven",
+    author_name: "Directed by Tony Braven",
     link: "https://www.youtube.com/watch?v=IbGcrrdDIQA",
   },
 ];
